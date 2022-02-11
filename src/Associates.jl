e8b8c345199708fd5c60c02d5b1408d23e3e0e17
Leo Breston <leo.breston@gmail.com>
2022-02-11 11:36:28 -0800

    hash dict pretty much working

--- src/Associates.jl
+++ src/Associates.jl
@@ -1,5 +1,57 @@
 module Associates
 
-# Write your package code here.
+# import InverseFunctions: inverse
+import Base: inv
+
+#export types
+export AssociativeHashMap
+
+#export functions
+export dom, image, preimage, codom, inverse, inv
+
+struct AssociativeHashMap
+    forward::Dict
+    backward::Dict
+end
+
+AssociativeHashMap(forward::Dict) = AssociativeHashMap(forward, invert(forward))
+
+(m::AssociativeHashMap)(x) = applymap(m, x)
+applymap(m::AssociativeHashMap, x) = m.forward[x]
+dom(m::AssociativeHashMap) = reduce(∪, keys(m.forward))
+inverse(m::AssociativeHashMap) = AssociativeHashMap(m.backward, m.forward)
+inv(m::AssociativeHashMap) = inverse(m)
+
+
+image(m::AssociativeHashMap, x) = unique(m.(x))
+image(m::AssociativeHashMap) = codom(m)
+preimage(m::AssociativeHashMap) = dom(m)
+preimage(m::AssociativeHashMap, x) = image(inverse(m), x)
+codom(m::AssociativeHashMap) = dom(inverse(m))
+
+
+function invert(D::Dict)
+    invD = Dict()
+    for k in keys(D)
+        if D[k] in keys(invD)
+           push!(invD[D[k]], k)
+        else
+            invD[D[k]] =[k]
+        end
+    end
+    K = typejoin(typeof.(keys(invD))...)
+    V = typejoin(typeof.(values(invD))...)
+    Dict{K,V}(invD)
+end
+
+hashkv(D) = Dict(hash(value) => hash(key) for (key, value) in D)
+
+
+
+
+
+
+
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using Associates
+import Associates: hashkv
 using Test
+
+    v = collect(1:1:26)
+    v2 = hash.(repeat(collect(1:1:13),2))
+    D1 = hashkv(Dict(zip('A':'Z',v)))
+    D2=Dict([(hash.(rand(26)).=>v2)...])
+    m1= AssociativeHashMap(D1)
+    m2= AssociativeHashMap(D2)
+    m2.forward
+    m2.backward
+    inv(m1)
 
 @testset "Associates.jl" begin
     # Write your tests here.
+
 end
