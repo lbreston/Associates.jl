6a3d164bf266ba0c7c8c1a8107a8b4cb9450459d
Leo Breston <leo.breston@gmail.com>
2022-02-11 13:37:11 -0800

    Composition/general keys/vals  now work

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,15 @@ using Accessors
 using Test
 
     v = collect(1:1:26)
-    v2 = hash.(repeat(collect(1:1:13),2))
-    D1 = hashkv(Dict(zip('A':'Z',v)))
-    D2=Dict([(hash.(rand(26)).=>v2)...])
-    m1= AssociativeHashMap(D1)
+v
+D1 = Dict(zip('A':'Z',v))
+D2 = Dict(zip(v,rand(26)))
+    m1= GMap(D1)
 
-    m1
-    m1
-
-    m2= AssociativeHashMap(D2)
-    m2.forward
-    m2.backward
-
+    dom(m1)
+    codom(m2)
+    m2= GMap(D2)
+    m2∘m1
     inv(m1).forward
     
 
