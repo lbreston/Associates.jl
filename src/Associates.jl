2d4fefc4d8730823825a2aa01bf20b4c247ff56a
Leo Breston <leo.breston@gmail.com>
2022-02-11 18:38:59 -0800

    commit

--- src/Associates.jl
+++ src/Associates.jl
@@ -1,34 +1,66 @@
 module Associates
 
 # import InverseFunctions: inverse
-import Base: inv
+import Base: inv, ∘, +, ×, \, /, ∪, ∩
 
 #export types
-export AssociativeHashMap
+export GMap
 
 #export functions
-export dom, image, preimage, codom, inverse, inv
-
-struct AssociativeHashMap
+export dom, image, preimage, codom, inverse, inv, ∘, compose, \, /, ∪, ∩
+struct GMap
     forward::Dict
     backward::Dict
 end
 
-AssociativeHashMap(forward::Dict) = AssociativeHashMap(forward, invert(forward))
+GMap(forward::Dict) = GMap(forward, invert(forward))
+GMap(fun::Function, d) = GMap(fun2dict(fun, d))
 
-(m::AssociativeHashMap)(x) = applymap(m, x)
-applymap(m::AssociativeHashMap, x) = m.forward[x]
-dom(m::AssociativeHashMap) = reduce(∪, keys(m.forward))
-inverse(m::AssociativeHashMap) = AssociativeHashMap(m.backward, m.forward)
-inv(m::AssociativeHashMap) = inverse(m)
+(m::GMap)(x) = applymap(m, x)
+applymap(m::GMap, x) = m.forward[x]
+dom(m::GMap) = keys(m.forward)
+inverse(m::GMap) = GMap(m.backward, m.forward)
+inv(m::GMap) = inverse(m)
 
 
-image(m::AssociativeHashMap, x) = unique(m.(x))
-image(m::AssociativeHashMap) = codom(m)
-preimage(m::AssociativeHashMap) = dom(m)
-preimage(m::AssociativeHashMap, x) = image(inverse(m), x)
-codom(m::AssociativeHashMap) = dom(inverse(m))
+image(m::GMap, x) = unique(m.(x))
+image(m::GMap) = codom(m)
+preimage(m::GMap) = dom(m)
+preimage(m::GMap, x) = image(inverse(m), x)
+codom(m::GMap) = dom(inverse(m))
 
+
+pull_back(m2,m1) = dom(m2) ∩ codom(m1) |> x -> preimage(m1, x) 
+push_forward(m2, m1) = m2.(m1.(pull_back(m2, m1)))
+
+compose(m2, m1) = GMap(Dict(zip(pull_back(m2, m1), push_forward(m2, m1))))
+
+(∘)(m2::GMap, m1::GMap) = compose(m2, m1)
+
+(∘)(s::Set, m1::GMap) = Set(S ∩ codom(m1))
+
+(∘)(m2::GMap, S::Set) = Set(image(m2, S ∩ dom(m2)))
+
+(∪)(m1::GMap, m2::GMap) = GMap(Dict(m1.forward ∪ m2.forward))
+
+(∩)(m1::GMap, m2::GMap) = GMap(Dict(m1.forward ∩ m2.forward))
+
+(/)(m1::GMap, m2::GMap) = m2∘inverse(m1)
+
+(\)(m1::GMap, m2::GMap) = inverse(m2)∘m1
+
+
+
+
+
+
+
+
+#some utilities 
+
+hashkv(D) = Dict(hash(value) => hash(key) for (key, value) in D)
+
+fun2dict(fun, d)= Dict(zip(d, map(fun, d)))
 
 function invert(D::Dict)
     invD = Dict()
@@ -36,7 +68,7 @@ function invert(D::Dict)
         if D[k] in keys(invD)
            push!(invD[D[k]], k)
         else
-            invD[D[k]] =[k]
+            invD[D[k]] = [k]
         end
     end
     for k in keys(invD)
@@ -46,16 +78,6 @@ function invert(D::Dict)
     V = typejoin(typeof.(values(invD))...)
     Dict{K,V}(invD)
 end
-
-
-#some utilities 
-hashkv(D) = Dict(hash(value) => hash(key) for (key, value) in D)
-
-
-
-
-
-
 
 
 
